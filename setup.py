173000e84977f0b413b5614a4e176ffb820eeede
Andy Wegner <andy@syntaxtechnology.com>
2015-12-09 10:44:25 -0600

    Initial Commit - 2

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+setup(name='SE Plagiarism Control Panel',
+      version='0.0.1',
+      description='A basic control panel utilized to monitor checks of SE Tag Wikis',
+      author='Andy Wegner',
+      author_email='se-andy@syntaxtechnology.com',
+      url='',
+     install_requires=['Flask>=0.10.1'],
      )
