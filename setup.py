5809fac614c823ea5f1e22ccbd000cc5a1cfde22
AWegnerGitHub <andy@andrewwegner.com>
2015-12-11 23:51:29 -0600

    Adjust imports to allow pytest>=2.8.0

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+import sys
 
 needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
 pytest_runner = ['pytest-runner'] if needs_pytest else []
@@ -10,7 +11,7 @@ setup(name='SE Plagiarism Control Panel',
       author_email='se-andy@syntaxtechnology.com',
       url='',
      install_requires=['Flask>=0.10.1', 'pandas>=0.17.1', 'simplejson==3.8.1', 'beautifulsoup4==4.4.0',
-	 'sqlalchemy==1.0.9','pymysql==0.6.7','flask-sqlalchemy==2.1','pytest==2.8.5','hypothesis==1.16.0',
+	 'sqlalchemy==1.0.9','pymysql==0.6.7','flask-sqlalchemy==2.1','pytest>=2.8.0','hypothesis==1.16.0',
 	 'pytest-runner==2.6.2'],
 	 setup_requires=[] + pytest_runner,
 	 tests_require=['pytest']
