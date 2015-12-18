abbed0830c930d1aec07a45f6f83b92156a1d412
AWegnerGitHub <andy@andrewwegner.com>
2015-12-17 23:15:01 -0600

    Tabs v spaces.

--- utils/utils.py
+++ utils/utils.py
@@ -5,13 +5,13 @@ def load_config(app):
     """
     Loads the prod/dev configuration based on the availablity of the 
     OPENSHIFT environment variables
-	
-	app: The Flask application variable
+    
+    app: The Flask application variable
     """
     if os.environ.get('OPENSHIFT_APP_UUID'):
         app.config.from_pyfile('flaskapp-prod.cfg')
-		app.config['CLIENT_SECRET'] = os.environ.get('CLIENT_SECRET')
+        app.config['CLIENT_SECRET'] = os.environ.get('CLIENT_SECRET')
     else:
         app.config.from_pyfile('flaskapp-dev.cfg')
-		import user_settings
-		app.config['CLIENT_SECRET'] = user_settings.CLIENT_SECRET
+        import user_settings
+        app.config['CLIENT_SECRET'] = user_settings.CLIENT_SECRET
